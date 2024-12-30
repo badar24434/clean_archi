fab11b1896732dba6401b8b11891fe3c92618294
badar24434 <164218215+badar24434@users.noreply.github.com>
2024-12-31 01:18:01 +0800

    network fixed

--- lib/features/pokemon/presentation/providers/pokemon_provider.dart
+++ lib/features/pokemon/presentation/providers/pokemon_provider.dart
@@ -1,4 +1,4 @@
-import 'package:data_connection_checker_tv/data_connection_checker.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -28,7 +28,7 @@ class PokemonProvider extends ChangeNotifier {
       remoteDataSource: PokemonRemoteDataSourceImpl(dio: Dio()),
       localDataSource: PokemonLocalDataSourceImpl(
           sharedPreferences: await SharedPreferences.getInstance()),
-      networkInfo: NetworkInfoImpl(DataConnectionChecker()),
+      networkInfo: NetworkInfoImpl(Connectivity()),
     );
 
     final failureOrPokemon = await GetPokemon(repository).call(
